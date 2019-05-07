94f42ee587236c853d0dff794f38f3c039b3c09c
Sarah Griffioen <32642974+slgriff27@users.noreply.github.com>
2019-05-06 23:34:21 -0400

    Update lab_1.pl

--- lab13/lab_1.pl
+++ lab13/lab_1.pl
@@ -1,8 +1,8 @@
-Exercise 13.1
+%Exercise 13.1
 
-a. Do these exercises from LPN!.
+%a. Do these exercises from LPN!.
 
-i. Exercise 3.2
+%i. Exercise 3.2
 
 directlyIn(katarina,olga).
 directlyIn(olga,natasha).
@@ -12,12 +12,12 @@ in(X,Y)  :-  directlyIn(X,Y).
 in(X,Y)  :-  directlyIn(X,Z),
              in(Z,Y).
 
-i. Exercise 4.5
+%i. Exercise 4.5
 
 listtran([],[]).
 listtran([X|Xs],[Y|Ys])  :-  trans(X,Y),
                              listtran(Xs,Ys).
 
-b.
+%b.
 
-Yes. It implements a generalized version of modus ponens because it allows for variables and instantiation. For instance, “loves(X,Y)  :-  father(X,Y).” is an example of a statement with variables. This statement means that if X is the father of Y, then X loves Y. Anything could be unified or instantiated with these two variables to make this statement true as long as both X’s are the same and both Y’s are the same.
+%Yes. It implements a generalized version of modus ponens because it allows for variables and instantiation. For instance, “loves(X,Y)  :-  father(X,Y).” is an example of a statement with variables. This statement means that if X is the father of Y, then X loves Y. Anything could be unified or instantiated with these two variables to make this statement true as long as both X’s are the same and both Y’s are the same.
